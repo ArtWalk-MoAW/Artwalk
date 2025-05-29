9165db0a91239e5db937417477775a70a7af14d3
Silvia Trovato <strovato@hm.edu>
2025-05-29 19:20:52 +0200

    #7 expo-location installiert und anfrage auf standort sowie ergeniss loggen

--- Frontend/artwalk-app/app/(tabs)/index.tsx
+++ Frontend/artwalk-app/app/(tabs)/index.tsx
@@ -1,89 +1,22 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { useState } from "react";
-import { TextInput, Button } from "react-native";
-import { StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text } from 'react-native';
+import useUserLocation from '../../hooks/useUserLocation';
 
+export default function LocationDebugScreen() {
+  const { location, errorMsg } = useUserLocation();
 
+  if (errorMsg) {
+    return <Text>{errorMsg}</Text>;
+  }
 
-export default function App() {
-  const [command, setCommand] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
-  const [host, setHost] = useState<string>('localhost:8080');
-
-  const sendCommand = async () => {
-      try {
-        const response = await fetch(`http://${host}/run`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ command }),
-      });
-        const data = await response.json();
-        setMessage(data.message);
-      } catch (error) {
-        console.error(error);
-        setMessage('Failed to execute command.');
-      }
-  };
+  if (!location) {
+    return <Text>Lade Standortdaten...</Text>;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Architecture Demo App</Text>
-      <Text style={styles.paragraph}>
-        This is a simple demo app to illustrate communication between frontend 
-        and backend.</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter a host and port"
-        value={host}
-        onChangeText={setHost}
-        autoCapitalize='none'
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter a command"
-        value={command}
-        onChangeText={setCommand}
-        autoCapitalize='none'
-      />
-      <Button title="Send" onPress={sendCommand} />
-      <Text style={styles.message}>{message}</Text>
+    <View>
+      <Text>Latitude: {location.coords.latitude}</Text>
+      <Text>Longitude: {location.coords.longitude}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFEFC',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    color: '#3A522D',
-  },
-  paragraph: {
-    fontSize: 16,
-    marginBottom: 20,
-    color: '#3A522D',
-  },
-  input: {
-    height: 40,
-    borderColor: '#3A522D',
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-    width: '80%',
-    backgroundColor: '#FFFFFF',
-  },
-  message: {
-    marginTop: 20,
-    fontSize: 16,
-    color: '#3A522D',
-  },
-});
--- Frontend/artwalk-app/app/(tabs)/saved.tsx
+++ Frontend/artwalk-app/app/(tabs)/saved.tsx
@@ -7,11 +7,49 @@ import { StyleSheet } from "react-native";
 
 
 export default function App() {
+  const [command, setCommand] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+  const [host, setHost] = useState<string>('localhost:8080');
 
+  const sendCommand = async () => {
+      try {
+        const response = await fetch(`http://${host}/run`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ command }),
+      });
+        const data = await response.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error(error);
+        setMessage('Failed to execute command.');
+      }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>My Saves</Text>
+      <Text style={styles.title}>Architecture Demo App</Text>
+      <Text style={styles.paragraph}>
+        This is a simple demo app to illustrate communication between frontend 
+        and backend.</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a host and port"
+        value={host}
+        onChangeText={setHost}
+        autoCapitalize='none'
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a command"
+        value={command}
+        onChangeText={setCommand}
+        autoCapitalize='none'
+      />
+      <Button title="Send" onPress={sendCommand} />
+      <Text style={styles.message}>{message}</Text>
     </View>
   );
 }
@@ -22,7 +60,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FFFEFC',
-    
   },
-  
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#3A522D',
+  },
+  paragraph: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: '#3A522D',
+  },
+  input: {
+    height: 40,
+    borderColor: '#3A522D',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '80%',
+    backgroundColor: '#FFFFFF',
+  },
+  message: {
+    marginTop: 20,
+    fontSize: 16,
+    color: '#3A522D',
+  },
 });
